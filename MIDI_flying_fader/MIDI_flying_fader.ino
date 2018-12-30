f4e542def961fdf1950d6736786442dc7487464c
saarts <saartssamo@gmail.com>
2018-12-31 00:03:01 +0200

    Working version with capacitive touch

--- MIDI_flying_fader/MIDI_flying_fader.ino
+++ MIDI_flying_fader/MIDI_flying_fader.ino
@@ -1,75 +1,79 @@
 /*
  *
  * Created: 22/Dec/2018
- * Modified: 24/Dec/2018
+ * Modified: 30/Dec/2018
  * 
  * NB! this code is using arduino micro with Atmega32u4
  *
  */ 
 
 //*****************************************************************************//
-
 #define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
-
-#define PitchBend 0xE0 
 
 #include "MIDIUSB.h"
 
+#include <CapacitiveSensor.h>
+
 //*****************************************************************************//
+
+CapacitiveSensor   cs_2_3 = CapacitiveSensor(2,3);
 
 //Pin change related
 uint8_t pinstates = 0b11111110;
 uint8_t dowork = 0;
-
-
 
 //software debounce related
 unsigned long curmillis = 0;
 unsigned long prevmillis = 0;
 unsigned long debmillis = 70;
 
+//Analog related
+const int threshold = 4;
+
+
 
 //
 unsigned long timemillis = 0;
-unsigned long delaymillis = 500;
+unsigned long delaymillis = 10;
 
 //Analog related
-const int threshold = 4;
 const uint8_t highspeed = 210;
 const uint8_t lowspeed = 120;
 const uint8_t lowspeedHR = 138;
 const uint8_t difference = 110;
 static int lastpotvalue = 0;
 static int lastmappedvalue = 0;
-int gotovalue=0;
+static int gotovalue=0;
 
 static uint8_t low, high;
 
+uint8_t follow = 1;
+uint8_t touch=0;
+uint8_t prevtouch=0;
+uint8_t loopcount=0;
 
+int analogbuffer = 0;
 
-
-  int faderChannel = 1;
-  bool positionUpdated = false;
 
 //*****************************************************************************//
 
-// Event type (0x09 = note on, 0x08 = note off, 0x0B = control change CC, 0x0C = program change  ).
+// Event type (0xE0= PitchBend 0x09 = note on, 0x08 = note off, 0x0B = control change CC, 0x0C = program change  ).
 // Channel can be anything between 0-15. Typically reported to the user as 1-16.
 // Pitch is the note number (48 = middle C).
 // Velocity (64 = normal, 127 = fastest).
 // Control is the CC command number.
 
 
-//
-//void noteon(byte channel, byte pitch, byte velocity) {
-//  midiEventPacket_t noteon = {0x09, 0x90 | channel, pitch, velocity};
-//  MidiUSB.sendMIDI(noteon);
-//}
-//
-//void noteoff(byte channel, byte pitch, byte velocity) {
-//  midiEventPacket_t noteoff = {0x08, 0x80 | channel, pitch, velocity};
-//  MidiUSB.sendMIDI(noteoff);
-//}
+
+void noteon(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteon = {0x09, 0x90 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteon);
+}
+
+void noteoff(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteoff = {0x08, 0x80 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteoff);
+}
 
 
 //*****************************************************************************//
@@ -88,12 +92,20 @@ void controlchange(byte channel, byte control, byte value) {
 }
 
 
+
+
+void pitchbend(byte channel, int value) {
+  byte lowValue = value & 0x7F;
+  byte highValue = value >> 7;
+  midiEventPacket_t event ={0x0E, 0xE0 | channel, lowValue, highValue};
+  MidiUSB.sendMIDI(event);
+  // MidiUSB.flush();
+}
+
+
 //*****************************************************************************//
 
 void setup() {
-
-
- //THIS SECTION IS FOR PIN CHANGE INTERRUPT
 
   //Port B pins on micro
   // PB0 = SS     PCINT0   There is RX LED conneced to this pin, therefore I do not use it. One could remove it from the board.
@@ -104,8 +116,6 @@ void setup() {
   // PB5 = D9     PCINT5
   // PB6 = D10    PCINT6
   // PB7 = D11    PCINT7
-
-  
   
                  // turn off global interrupts  I-bit in the Status Register (SREG)
   DDRB = B00000110;    //Port B inputs
@@ -119,14 +129,13 @@ void setup() {
 //Change Interrupt Request is executed from the PCI0 Interrupt Vector. PCINT7..0 pins are enabled individually
 //by the PCMSK0 Register.
 
-  PCMSK0 |= B00000000;  // Choose which pins output interrupt
+  PCMSK0 |= B11111110;  // Choose which pins output interrupt
 //Each PCINT7..0 bit selects whether pin change interrupt is enabled on the corresponding I/O pin. If PCINT7..0
 //is set and the PCIE0 bit in PCICR is set, pin change interrupt is enabled on the corresponding I/O pin. If
 //PCINT7..0 is cleared, pin change interrupt on the corresponding I/O pin is disabled.
 //The 0 bit is 0, because of the RX LED.
   
-// turn on global interrupts  I-bit in the Status Register (SREG)
-
+                   // turn on global interrupts  I-bit in the Status Register (SREG)
 
  //THIS SECTION IS FOR ADC FUNCTIONS
 
@@ -158,115 +167,102 @@ ADCSRB= B00000000;
 
 DIDR0 = B00000001;
 
-
-
-
-
-
-
- 
- // PORTB = B00000010; //DOWN
-  //delay(100);
-  //PORTB = B00000100; //UP
-  //delay(100);
-
-  
-  analogWrite(5, 140);
-
-
-
- // Serial.begin(115200);
 }
+
+
 
 
 //*****************************************************************************//
 int analogvalue(){
-  
- // uint8_t low, high;
-  
-  ADMUX = B01000000;
-  //ADCSRA |= B01000000;
 
+  ADMUX = B01000000;
+  
   if(CHECK_BIT(ADCSRA,6)){
     }else{
         low  = ADCL;
   high = ADCH;
   ADCSRA |= B01000000;
       }
-
-
-
-  
   return (high << 8) | low;
   
   }
 
-
-
 //*****************************************************************************//
 
-
 void loop() {
- // delay(100);
- // PORTB = B00000000;
+
+// Event type (0x09 = note on, 0x08 = note off, 0x0B = control change CC, 0x0C = program change  ).
+ midiEventPacket_t rx;
+  do {
+    rx = MidiUSB.read();
+    if (rx.header != 0) {
+      if(rx.byte1 == 0xE0){
+        gotovalue= ((rx.byte3 << 7) | rx.byte2)*0.0625;
+        }
+    }
+  } while (rx.header != 0);
+
+
+
+  
+   if(millis()-timemillis>=delaymillis){
+     timemillis=millis();
+     int sensorval = cs_2_3.capacitiveSensor(30);
+     
+      if(sensorval>450){
+        touch=1;
+        
+        }else{
+          touch=0;
+
+          }
+      
+               if(touch==0){
+                  follow = 1;
+                  noteon(0x00, 0x68, 0x00);
+              }else if(touch==1){
+                  PORTB = B00000000;
+                  follow = 0; 
+                  noteon(0x00, 0x68, 0x7f);
+                }
+
+
+
+if(follow <1){
+   int currentpotvalue = analogvalue();
+    if(currentpotvalue!=lastpotvalue){
+    lastpotvalue = currentpotvalue;
+    pitchbend(0, currentpotvalue*16);
+    }
+  }
+ 
+ 
  
   
-  
-
     
-   if(millis()-timemillis>=delaymillis){
-
-       gotovalue=gotovalue+100;
-    
-                if(gotovalue>1000){
-      gotovalue=20;
-      }
-
-   // Serial.print("gotoval: ");
-  //  Serial.println(gotovalue);
-  //  Serial.print(" potval: ");
-   // Serial.println(analogvalue());
-    timemillis=millis();
-
-
-    }
-
-  int currentpotvalue = analogvalue();
-
-    
+  if(follow >0){
+     int currentpotvalue = analogvalue();
    if(currentpotvalue>(gotovalue+threshold)){
-
     PORTB = B00000010; //DOWN
-
     if((currentpotvalue-gotovalue)>difference){
       analogWrite(5, highspeed);
       }else{
         analogWrite(5, lowspeed);
-         
         }
-   // Serial.println("DOWN");
-   // delay(1);
     }else{
-            if(currentpotvalue<(gotovalue-threshold)){
-    PORTB = B00000100; //DOWN
-
+      if(currentpotvalue<(gotovalue-threshold)){
+         PORTB = B00000100; //UP
         if((gotovalue-currentpotvalue)>difference){
-      analogWrite(5, highspeed);
-      }else{
-        analogWrite(5, lowspeed);
-          
-        }
-   // delay(1);
+           analogWrite(5, highspeed);
+           }else{
+            analogWrite(5, lowspeed);   
+         }
     }else{
-    PORTB = B00000000;  
-   // delay(1);
+    PORTB = B00000000; 
       } 
       }
+      }
 
-
-   //Serial.println(currentpotvalue);
-  // delay(500);
- 
 
 if(dowork>0){
   curmillis = millis();
@@ -275,19 +271,26 @@ if(dowork>0){
   //If button is pressed, the pin is pulled LOW. For example, if pin 4 is pressed, PINB = 11101111
   //Check pin state against bits. Here the logic, is, when true, check the next one, if not, button is pressed.
   if(pinstates & 0b00000010){
-    }else{
-      controlchange(0, 30, 100);
-    //  Serial.println(1);
-      }
-      //debounce end
-      prevmillis=curmillis;
-  }
       
       //Work is done
       dowork = 0; 
 }
   
 }
+}
+}
+
+
+//*****************************************************************************//
+
+//Routine for pin change interrupt
+ISR(PCINT0_vect){ 
+//Record current pin states on port B.
+pinstates = PINB;
+//Flag for loop
+dowork = 1;
+
+  }
 
 
 //*****************************************************************************//
