1a0fbdd7550f4598ed3ecea7d511e3afb0e16954
saarts <saartssamo@gmail.com>
2019-01-10 11:42:52 +0200

    Create EEPROMreadwrite.ino

---
+++ EEPROMreadwrite.ino
@@ -0,0 +1,27 @@
+
+void EEPROM_write(unsigned int uiAddress, unsigned char ucData)
+{
+/* Wait for completion of previous write */
+while(EECR & (1<<EEPE))
+;
+/* Set up address and Data Registers */
+EEAR = uiAddress;
+EEDR = ucData;
+/* Write logical one to EEMPE */
+EECR |= (1<<EEMPE);
+/* Start eeprom write by setting EEPE */
+EECR |= (1<<EEPE);
+}
+
+unsigned char EEPROM_read(unsigned int uiAddress)
+{
+/* Wait for completion of previous write */
+while(EECR & (1<<EEPE))
+;
+/* Set up address register */
+EEAR = uiAddress;
+/* Start eeprom read by writing EERE */
+EECR |= (1<<EERE);
+/* Return data from Data Register */
+return EEDR;
+}
